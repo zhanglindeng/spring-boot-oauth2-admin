ee7356aff7447d91f42b86b4ea4f3e0fd92ee783
zhanglindeng <zhanglindeng@outlook.com>
2018-06-04 12:26:29 +0800

    init

---
+++ src/main/java/com/dzlin/oauth2/admin/Oauth2AdminApplication.java
@@ -0,0 +1,12 @@
+package com.dzlin.oauth2.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2AdminApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dzlin/oauth2/admin/Oauth2AdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dzlin.oauth2.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2AdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
